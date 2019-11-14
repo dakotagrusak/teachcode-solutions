0690bd366407f055a46f291b357f891003f6c248
Dakota Grusak <dgrusak@trinity.edu>
2019-11-14 09:54:23 -0600

    solution for task-2

---
+++ task2.py
@@ -0,0 +1,13 @@
+# Task 2:-
+#  Consider these variable names:-
+#  variable
+print("variable")
+
+#  sum@
+
+#  product_of_12
+print("product_of_12")
+
+#  12_product
+
+#  Just print out the correct variable names in order
